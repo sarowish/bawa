f5cf0db0be3c730abaaf5c97ffd0791594bb9af7
Berke Enercan <berkeenercan@tutanota.com>
2024-12-24 01:34:07 +0300

    fix(windows): handle rename events

--- src/watcher.rs
+++ src/watcher.rs
@@ -11,6 +11,45 @@ pub struct Watcher(pub RecommendedWatcher);
 
 impl Watcher {
     pub fn new(tx: UnboundedSender<Event>) -> Result<Self> {
+        #[cfg(windows)]
+        let watcher =
+            notify::recommended_watcher(move |res: Result<notify::Event, notify::Error>| {
+                use std::sync::Mutex;
+                static RENAME_FROM: Mutex<Option<PathBuf>> = Mutex::new(None);
+
+                let Ok(event) = res else { return };
+
+                match event.kind {
+                    EventKind::Create(_)
+                    | EventKind::Modify(ModifyKind::Name(RenameMode::To))
+                    | EventKind::Remove(_) => (),
+                    EventKind::Modify(ModifyKind::Name(RenameMode::From)) => {
+                        *RENAME_FROM.lock().unwrap() = Some(event.paths[0].clone());
+                        return;
+                    }
+                    _ => return,
+                }
+
+                let file_name = event.paths[0].file_name().unwrap().to_string_lossy();
+                if file_name == "selected_save_file" || file_name == "selected_profile" {
+                    *RENAME_FROM.lock().unwrap() = None;
+                    return;
+                }
+
+                if let EventKind::Modify(ModifyKind::Name(RenameMode::To)) = event.kind {
+                    tx.send(Event::FileSystem(FileSystemEvent::from_modify(
+                        event,
+                        RENAME_FROM.lock().unwrap().as_ref().unwrap(),
+                    )))
+                    .unwrap();
+                    *RENAME_FROM.lock().unwrap() = None;
+                } else {
+                    tx.send(Event::FileSystem(FileSystemEvent::from(event)))
+                        .unwrap();
+                }
+            })?;
+
+        #[cfg(unix)]
         let watcher =
             notify::recommended_watcher(move |res: Result<notify::Event, notify::Error>| {
                 let Ok(event) = res else { return };
@@ -63,6 +102,28 @@ pub enum Kind {
     Create,
     Rename(PathBuf),
     Delete,
+}
+
+impl FileSystemEvent {
+    #[cfg(windows)]
+    fn from_modify(event: NotifyEvent, from: &Path) -> Self {
+        let kind = Kind::Rename(event.paths[0].clone());
+        let path = from.to_path_buf();
+        let context = if utils::get_relative_path(&utils::get_data_dir().unwrap(), &path)
+            .unwrap()
+            .contains(MAIN_SEPARATOR)
+        {
+            Context::Entry
+        } else {
+            Context::Profile
+        };
+
+        Self {
+            context,
+            kind,
+            path,
+        }
+    }
 }
 
 impl From<NotifyEvent> for FileSystemEvent {
